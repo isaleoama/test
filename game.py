1d5a2b9776a7c39356b3abd978a403c69c030754
isa <alfred.nikitin@Mail.ru>
2024-10-27 17:58:07 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,26 +1,37 @@
-import arcade
+import bs4
+import requests
+from bs4 import BeautifulSoup
+import fake_useragent
+import xlsxwriter
+
+user = fake_useragent.UserAgent().random
+headers = {'user-agent': user}
+main_url = 'https://re-store.ru/'
+data = [['Наименование', 'Цена', 'Ссылка', 'Картинка']]
 
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Pong Game"
-
-class Bar(arcade.Sprite):
-    def __init__(self):
-        super().__init__('bar.png', 1.0)
+def get_soup(url):
+    res = requests.get(url, headers)
+    return bs4.BeautifulSoup(res.text, 'html.parser')
 
 
+categories_page = get_soup(main_url + '/apple-iphone/')
+categories = categories_page.find_all('div', class_="catalog__products")
+for cat in categories:
+    subcategories_page = get_soup(main_url + cat['href'])
+    subcategories = subcategories_page.find_all('a', class_="product-card__link")
+    for subcat in subcategories:
+        iphones_page = get_soup(main_url + subcat['href'])
+        iphones = iphones_page.find_all('div', class_="product-card__wrap")
+        for iphone in iphones:
+            title = iphone.find('div', class_="product-card__title").strip()
+            price = iphone.find('div', class_="product-card__prices").find(text=True).strip()
+            url = iphone.find('a'), ['href'].strip()
+            img = iphone.find('div', class_='image')['style'].split('url(')[1].split(')')[0].replace('/tn/', '/source/')
+            data.append([title, price, main_url + url, main_url + img])
 
-class Game(arcade.Window):
-    def __init__(self, width, height, title):
-        super().__init__(width, height, title)
-        self.bar = Bar()
+with xlsxwriter.Workbook('iphones.xlsx') as workbook:
+    worksheet = workbook.add_worksheet()
 
-
-
-    def on_draw(self):
-        self.clear((255,255,255))
-        self.bar.draw()
-if __name__ == "__main__":
-    window = Game(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    arcade.run()
+    for row_num, info in enumerate(data):
+        worksheet.write_row(row_num, 0, info)
