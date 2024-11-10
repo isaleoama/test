96aebcaa6bc0c9d03d353ecb64609f89ca10972a
isa <alfred.nikitin@Mail.ru>
2024-11-10 13:47:20 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,37 +1,119 @@
-import bs4
-import requests
-from bs4 import BeautifulSoup
-import fake_useragent
-import xlsxwriter
+import time
+import json
+from pynput import mouse,keyboard
 
-user = fake_useragent.UserAgent().random
-headers = {'user-agent': user}
-main_url = 'https://re-store.ru/'
-data = [['Наименование', 'Цена', 'Ссылка', 'Картинка']]
+print("Hold right click for 2 seconds then release to end the recording for mouse")
+print("Click 'ESC' to end the recording for keyboard")
+print("Both are needed to finish recording")
 
 
-def get_soup(url):
-    res = requests.get(url, headers)
-    return bs4.BeautifulSoup(res.text, 'html.parser')
+recording = []
+count = 0
 
 
-categories_page = get_soup(main_url + '/apple-iphone/')
-categories = categories_page.find_all('div', class_="catalog__products")
-for cat in categories:
-    subcategories_page = get_soup(main_url + cat['href'])
-    subcategories = subcategories_page.find_all('a', class_="product-card__link")
-    for subcat in subcategories:
-        iphones_page = get_soup(main_url + subcat['href'])
-        iphones = iphones_page.find_all('div', class_="product-card__wrap")
-        for iphone in iphones:
-            title = iphone.find('div', class_="product-card__title").strip()
-            price = iphone.find('div', class_="product-card__prices").find(text=True).strip()
-            url = iphone.find('a'), ['href'].strip()
-            img = iphone.find('div', class_='image')['style'].split('url(')[1].split(')')[0].replace('/tn/', '/source/')
-            data.append([title, price, main_url + url, main_url + img])
+def on_press(key):
+    try:
+        json_object = {
+            'action': 'pressed_key',
+            'key': key.char,
+            '_time': time.time()
+        }
+    except AttributeError:
+        if key == keyboard.Key.esc:
+            print("Keyboard recording ended.")
+            return False
 
-with xlsxwriter.Workbook('iphones.xlsx') as workbook:
-    worksheet = workbook.add_worksheet()
+        json_object = {
+            'action': 'pressed_key',
+            'key': str(key),
+            '_time': time.time()
+        }
 
-    for row_num, info in enumerate(data):
-        worksheet.write_row(row_num, 0, info)
+    recording.append(json_object)
+
+
+def on_release(key):
+    try:
+        json_object = {
+            'action': 'released_key',
+            'key': key.char,
+            '_time': time.time()
+        }
+    except AttributeError:
+        json_object = {
+            'action': 'released_key',
+            'key': str(key),
+            '_time': time.time()
+        }
+
+    recording.append(json_object)
+
+
+def on_move(x, y):
+    if len(recording) >= 1:
+        if (recording[-1]['action'] == "pressed" and \
+            recording[-1]['button'] == 'Button.left') or \
+                (recording[-1]['action'] == "moved" and \
+                 time.time() - recording[-1]['_time'] > 0.02):
+            json_object = {
+                'action': 'moved',
+                'x': x,
+                'y': y,
+                '_time': time.time()
+            }
+
+            recording.append(json_object)
+
+
+def on_click(x, y, button, pressed):
+    json_object = {
+        'action': 'clicked' if pressed else 'unclicked',
+        'button': str(button),
+        'x': x,
+        'y': y,
+        '_time': time.time()
+    }
+
+    recording.append(json_object)
+
+    if len(recording) > 1:
+        if recording[-1]['action'] == 'unclicked' and \
+                recording[-1]['button'] == 'Button.right' and \
+                recording[-1]['_time'] - recording[-2]['_time'] > 2:
+            with open('recording.json', 'w') as f:
+                json.dump(recording, f)
+            print("Mouse recording ended.")
+            return False
+
+
+def on_scroll(x, y, dx, dy):
+    json_object = {
+        'action': 'scroll',
+        'vertical_direction': int(dy),
+        'horizontal_direction': int(dx),
+        'x': x,
+        'y': y,
+        '_time': time.time()
+    }
+
+    recording.append(json_object)
+
+
+def start_recording():
+    keyboard_listener = keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release)
+
+    mouse_listener = mouse.Listener(
+        on_click=on_click,
+        on_scroll=on_scroll,
+        on_move=on_move)
+
+    keyboard_listener.start()
+    mouse_listener.start()
+    keyboard_listener.join()
+    mouse_listener.join()
+
+
+if __name__ == "__main__":
+    start_recording()
